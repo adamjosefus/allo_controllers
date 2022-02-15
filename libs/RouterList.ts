36e2cc6e18b3d156744f5d62d0916150d885d7f7
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-15 21:35:46 +0100

    Create RouterList.ts

---
+++ libs/RouterList.ts
@@ -0,0 +1,15 @@
+/**
+ * @copyright Copyright (c) 2022 Adam Josefus
+ */
+
+
+import { RouterList as RouterListParent, type RouterOptions } from "https://deno.land/x/allo_routing@v1.0.1/mod.ts";
+
+
+export class RouterList extends RouterListParent {
+
+    constructor(options?: RouterOptions) {
+        super(options);
+    }
+
+}
