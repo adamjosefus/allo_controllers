3708a5e1b87c3915c749a493b93725a88399a4be
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-18 10:15:16 +0100

    Update RouterList.ts
    Add ControllerManher

--- libs/RouterList.ts
+++ libs/RouterList.ts
@@ -4,12 +4,18 @@
 
 
 import { RouterList as SuperRouterList, type RouterOptions } from "https://deno.land/x/allo_routing@v1.0.1/mod.ts";
+import { ControllerManager } from "./ControllerManager.ts";
 
 
 export class RouterList extends SuperRouterList {
 
-    constructor(options?: RouterOptions) {
+    readonly #manager: ControllerManager;
+
+
+    constructor(manager: ControllerManager, options?: RouterOptions) {
         super(options);
+
+        this.#manager = manager;
     }
 
 }
