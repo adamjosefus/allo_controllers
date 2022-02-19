a29115005a3d2cfa9dc6ec2f1ee208dda36993cb
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-19 12:12:59 +0100

    Create ControllerExit.ts

---
+++ libs/ControllerExit.ts
@@ -0,0 +1,20 @@
+/**
+ * @copyright Copyright (c) 2022 Adam Josefus
+ */
+
+
+type Promisable<T> = T | Promise<T>;
+type ExitValueType = Promisable<Response | Deno.FsFile | File | string | number>;
+
+
+export class ControllerExit extends Error {
+
+    readonly value: ExitValueType;
+
+    constructor(value: ExitValueType) {
+        super(`Controller Exit - This exception should be caught.`);
+
+        this.value = value;
+    }
+
+}
