a5189aba832345df5f1eb3ca36882d098c93319b
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-23 19:45:58 +0100

    Update ControllerManager.ts

--- libs/ControllerManager.ts
+++ libs/ControllerManager.ts
@@ -4,19 +4,27 @@
 
 
 import { Cache } from "https://deno.land/x/allo_caching@v1.0.2/mod.ts";
-import { Controller as AbstractController } from "./Controller.ts";
-import { ControllerLifeCycle } from "./ControllerLifeCycle%20.ts";
+import { ControllerLifeCycle } from "./ControllerLifeCycle.ts";
 import { ControllerLoader } from "./ControllerLoader.ts";
+import { DIContainer } from "./DIContainer.ts";
+import { Case } from "./helper/Case.ts";
 
 
-class Controller extends AbstractController { }
-
+type MetaType = {
+    controller: string,
+    action: string,
+}
 
 
 export class ControllerManager {
 
     readonly #loader: ControllerLoader;
+    readonly #di: DIContainer;
+
     readonly #callerCache: Cache<ControllerLifeCycle> = new Cache();
+    readonly #metaCache: Cache<MetaType> = new Cache();
+    readonly #paramControllerCache: Cache<string> = new Cache();
+    readonly #paramActionCache: Cache<string> = new Cache();
 
     readonly defaultControllerFallback = 'Homepage';
     readonly defaultActionFallback = 'default';
@@ -24,32 +32,56 @@ export class ControllerManager {
 
     constructor(dir: string) {
         this.#loader = new ControllerLoader(dir);
+        this.#di = new DIContainer()
     }
 
 
-    #parseMeta(meta: string) {
-        const [controller, action] = meta.split(":");
-
-        // TODO: check case
-
-        return {
-            controller: firstUpper(controller),
-            action: firstLower(action),
-        };
+    // deno-lint-ignore ban-types
+    addDependency(name: string, instance: Object) {
+        this.#di.add(name, instance);
     }
 
 
     async createResponse(meta: string, req: Request, params: Record<string, string>): Promise<Response> {
-        const metaParts = this.#parseMeta(meta);
+        const pointer = this.#parseMeta(meta);
+        const paramMap = new Map(Object.entries(params));
 
-        const controllerName = params['controller'] ?? metaParts.controller;
-        const action = params['action'] ?? metaParts.action;
+        const controller = ((s) => {
+            if (s) return this.#paramControllerCache.load(s, () => Case.kebabToPascal(s));
+            else return pointer.controller;
+        })(paramMap.get('controller'));
 
-        const controller = await this.#loader.createInstanceObject(controllerName, req);
+        const action = ((s) => {
+            if (s) return this.#paramActionCache.load(s, () => Case.kebabToCamel(s));
+            else return pointer.action;
+        })(paramMap.get('action'));
 
-        const lifeCycle = this.#callerCache.load(meta, () => new ControllerLifeCycle(controller));
+        const instance = await this.#loader.createInstanceObject(controller, req);
+        const lifeCycle = this.#callerCache.load(meta, () => new ControllerLifeCycle(instance));
 
-        return lifeCycle.call(di, controller, action, params);
+        console.log("> instance", instance);
+        console.log("> lifeCycle", lifeCycle);
 
+        return lifeCycle.call(this.#di, instance, action, params);
+    }
+
+
+    #parseMeta(meta: string) {
+        return this.#metaCache.load(meta, () => {
+            const [controller, action] = meta.split(":");
+
+            if (!Case.isPascal(controller)) {
+                throw new Error(`Invalid controller name: ${controller}. Case must be Pascal`);
+            }
+
+            if (!Case.isCamal(action)) {
+                throw new Error(`Invalid action name: ${action}. Case must be Camal`);
+            }
+
+            return {
+                controller,
+                action,
+            };
+        });
     }
 }
