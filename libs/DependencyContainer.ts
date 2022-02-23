0535d349dd25ed9e140ae6b8e53fd1e78964a60c
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-02-23 18:51:01 +0100

    Create DependencyContainer.ts

---
+++ libs/DependencyContainer.ts
@@ -0,0 +1,30 @@
+import { Case } from "./helper/Case.ts";
+
+
+
+export class DependencyContainer {
+
+    #dependecies: Map<string, unknown> = new Map();
+
+    // deno-lint-ignore ban-types
+    register(name: string, instance: Object): void {
+        if (!Case.isPascal(name)) {
+            throw new Error(`Invalid dependency name: ${name}. Case must be Pascal`);
+        }
+
+        if (this.#dependecies.has(name)) {
+            throw new Error(`Dependecy "${name}" already exists`);
+        }
+
+        this.#dependecies.set(name, instance);
+    }
+
+
+    get<T = unknown>(name: string): T {
+        if (!this.#dependecies.has(name)) {
+            throw new Error(`Dependecy "${name}" not found`);
+        }
+
+        return this.#dependecies.get(name)! as T;
+    }
+}
