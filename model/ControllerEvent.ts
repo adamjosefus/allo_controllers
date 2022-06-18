7cb7b34da812187318e0fe87d81c5b2497ce2903
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-06-18 22:39:18 +0200

    Update ControllerEvent.ts

--- model/ControllerEvent.ts
+++ model/ControllerEvent.ts
@@ -9,7 +9,9 @@ import { Controller } from "./Controller.ts";
 export class ControllerEvent<C extends Controller, T extends string> extends CustomEvent<{ controller: C }> {
     constructor(type: T, controller: C) {
         super(type, {
-            detail: { controller }
+            detail: {
+                controller
+            },
         });
     }
 }
