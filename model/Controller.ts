b9bc6c0939328e88af86866670ffa556bebe84bd
Adam Josefus <1031370+adamjosefus@users.noreply.github.com>
2022-06-18 22:39:23 +0200

    Update Controller.ts

--- model/Controller.ts
+++ model/Controller.ts
@@ -23,7 +23,9 @@ interface IController<T extends string = 'startup' | 'render' | 'shutdown'> {
 
 export abstract class Controller extends EventTarget implements IController {
 
-    readonly #request: Request;
+    readonly #httpRequest: Request;
+    #httpResponse: Response | undefined;
+
     readonly #action: string;
     readonly #params: Record<string, string>;
 
@@ -33,60 +35,124 @@ export abstract class Controller extends EventTarget implements IController {
     constructor(request: Request, action: string, params: Record<string, string>) {
         super();
 
-        this.#request = request;
+        this.#httpRequest = request;
+        this.#httpResponse = undefined;
+
         this.#action = action;
         this.#params = params;
     }
 
 
+    // #region — Common LyfeCycle
+    /**
+     * This method is part of the life cycle.
+     * 
+     * If you don't call `super.startup()`, then override this method.
+     */
     startup(): void {
     }
 
 
+    /**
+     * This method is part of the life cycle.
+     * 
+     * If you don't call `super.beforeRender()`, then override this method.
+     */
     beforeRender(): void {
     }
 
 
+    /**
+     * This method is part of the life cycle.
+     * 
+     * If you don't call `super.afterRender()`, then override this method.
+     */
     afterRender(): void {
     }
 
 
+    /**
+     * This method is part of the life cycle.
+     * 
+     * If you don't call `super.shutdown()`, then override this method.
+     */
     shutdown(): void {
+    }
+    // #endregion
+
+
+    /**
+     * Get name of requested action of controller.
+     */
+    getAction(): string {
+        return this.#action;
     }
 
 
+    /**
+     * Manually change view from the controller.
+     * 
+     * No redirection occurs.
+     */
     setView(view: string): void {
         this.#forceView = view;
     }
 
 
+    /**
+     * Get name of requested view of controller.
+     */
     getView(): string {
         return this.#forceView ?? this.#action;
     }
 
 
+    // #region — Http
+    /**
+     * Returns the http request object.
+     */
     getHttpRequest(): Request {
-        return this.#request;
+        return this.#httpRequest;
     }
 
 
-    sendResponse(response: Response | Promise<Response>): void {
+    /**
+     * Returns sended response or undefined if no response was sended.
+     */
+    getHttpResponse(): Response | undefined {
+        return this.#httpResponse;
+    }
+    // #endregion
+
+
+    // #region — Send Response
+    sendResponse(response: Response): void {
+        this.#httpResponse = response;
         throw new ControllerLifeCycleExit(this, response);
     }
 
 
+    /**
+     * Create and send data as json response.
+     */
     sendJson(data: unknown, prettyPrint = false): void {
         const response = new JsonResponse(data, {}, prettyPrint);
         this.sendResponse(response);
     }
 
 
+    /**
+     * Create and send data as text response.
+     */
     sendText(text: string): void {
         const response = new TextResponse(text);
         this.sendResponse(response);
     }
 
 
+    /**
+     * Create and send data as file response.
+     */
     sendFile(file: string | File | Deno.FsFile): void {
         const response = new FileResponse(file);
         this.sendResponse(response);
@@ -99,8 +165,10 @@ export abstract class Controller extends EventTarget implements IController {
 
         this.sendResponse(response);
     }
+    // #endregion
 
 
+    // #region — Redirection
     redirectUrl(url: string): void {
         this.sendResponse(Response.redirect(url, Status.S302_Found));
     }
@@ -118,4 +186,5 @@ export abstract class Controller extends EventTarget implements IController {
     // redirectPermanent(meta: string): void {
     //     throw new Error("Not implemented");
     // }
+    // #endregion
 }
